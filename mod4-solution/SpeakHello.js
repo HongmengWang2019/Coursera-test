f3a6cc75591a2d7d33463ed9c1e468408137d64d
MiniMandi <whm2016as@gmail.com>
2019-09-01 22:08:45 +0800

    Small additions.

--- mod4-solution/SpeakHello.js
+++ mod4-solution/SpeakHello.js
@@ -1,4 +1,12 @@
-var speakWord = "Hello";
-function speak(name) {
+(function (window) {
+  var helloSpeaker = {};
+  
+  var speakWord = "Hello ";
+
+  helloSpeaker.speak = function speak(name) {
   console.log(speakWord + " " + name);
 }
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
